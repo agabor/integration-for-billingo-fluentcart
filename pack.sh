78c9561b7abad9a27e4931ac9a7b87b4af4a5e43
Gabor Angyal <gabor.angyal@codesharp.dev>
2025-11-22 14:20:39 +0100

    fix packing and publishing

--- pack.sh
+++ pack.sh
@@ -3,6 +3,7 @@ cd ..
 zip integration-for-billingo-fluentcart/integration-for-billingo-fluentcart.zip -r integration-for-billingo-fluentcart \
    --exclude="integration-for-billingo-fluentcart/.git/*" \
    --exclude="integration-for-billingo-fluentcart/tests/*" \
+   --exclude="integration-for-billingo-fluentcart/assets/*" \
    --exclude="integration-for-billingo-fluentcart/*.zip" \
    --exclude="integration-for-billingo-fluentcart/*.md" \
    --exclude="integration-for-billingo-fluentcart/*.sh"
